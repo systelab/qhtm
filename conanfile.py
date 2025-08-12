fe5b13d55a8769a40d4f82347dd0801eca800191
Francisco Puerta <fpuerta@werfen.com>
2025-08-12 12:18:23 +0200

    fix conanfile to correctly package required headers

--- conanfile.py
+++ conanfile.py
@@ -13,14 +13,14 @@ class QhtmLibConan(ConanFile):
 
 
     def package(self):
-        self.copy("*.h", dst="include/qhtm", src="../qhtm")
-        self.copy("array.h", dst="include/qhtm", src="../")
-        self.copy("common.h", dst="include/qhtm", src="../")
-        self.copy("map.h", dst="include/qhtm", src="../")
-        self.copy("mapiter.h", dst="include/qhtm", src="../")
-        self.copy("stack.h", dst="include/qhtm", src="../")
-        self.copy("windowtext.h", dst="include/qhtm", src="../")
-        self.copy("winhelper.h", dst="include/qhtm", src="../")
+        self.copy("*.h", dst="include/qhtm", src="qhtm")
+        self.copy("array.h", dst="include/qhtm")
+        self.copy("common.h", dst="include/qhtm")
+        self.copy("map.h", dst="include/qhtm")
+        self.copy("mapiter.h", dst="include/qhtm")
+        self.copy("stack.h", dst="include/qhtm")
+        self.copy("windowtext.h", dst="include/qhtm")
+        self.copy("winhelper.h", dst="include/qhtm")
         self.copy("*.lib", dst="lib", src=("lib/%s" % self.settings.build_type))
         self.copy("*.pdb", dst="lib", src=("lib/%s" % self.settings.build_type))
 
