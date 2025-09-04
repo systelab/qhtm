9dda64da54d71135796cb5e563e22ec22202ca9f
Luis Ruiz Mediavilla <lruiz@werfen.com>
2025-09-04 14:17:43 +0200

    use profile for host instead of build

--- generate_stub_packages.bat
+++ generate_stub_packages.bat
@@ -1,7 +1,7 @@
-conan export-pkg . --name=qhtm --version=1.8.0 --profile:build=.conanprofile -s build_type=Debug
-conan export-pkg . --name=qhtm --version=1.8.0 --profile:build=.conanprofile -s build_type=Release
+conan export-pkg . --name=qhtm --version=1.8.0 --profile:host=.conanprofile -s build_type=Debug -of int
+conan export-pkg . --name=qhtm --version=1.8.0 --profile:host=.conanprofile -s build_type=Release -of int
 
-conan export-pkg libpng/ --name=libpng --version=1.2.4 --profile:build=.conanprofile -s build_type=Debug
-conan export-pkg libpng/ --name=libpng --version=1.2.4 --profile:build=.conanprofile -s build_type=Release
-conan export-pkg jpeg-6b/ --name=libjpeg --version=6b --profile:build=.conanprofile -s build_type=Debug
-conan export-pkg jpeg-6b/ --name=libjpeg --version=6b --profile:build=.conanprofile -s build_type=Release
+conan export-pkg libpng/ --name=libpng --version=1.2.4 --profile:host=.conanprofile -s build_type=Debug
+conan export-pkg libpng/ --name=libpng --version=1.2.4 --profile:host=.conanprofile -s build_type=Release
+conan export-pkg jpeg-6b/ --name=libjpeg --version=6b --profile:host=.conanprofile -s build_type=Debug
+conan export-pkg jpeg-6b/ --name=libjpeg --version=6b --profile:host=.conanprofile -s build_type=Release
